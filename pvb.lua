a94dddc19e1bcea522157b3a6a6ac231d726b87e
Nghithanh491 <nghithanhau@gmail.com>
2025-09-28 15:38:07 +0700

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -1,31 +1,26 @@
-return {
+local publicConfig = {
     AUTO_UPDATE_RESTART = true,
     MAX_FPS = 3,  -- This will override setfpscap()
     LOW_CPU = true,
-    MAX_REBIRTH = 2,
+    MAX_REBIRTH = 99,
+    FROST_GRENADE_TARGET_MAX_HP = 100000,  -- Use frost grenade 100k+ hp brainrot
     BRAINROT_RARITY_STOP_REBIRTH = {"Secret", "Limited"},
+    OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
+    FUSE_PLANT = {"Mr Carrot"},
 
-    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio"},
-    BUY_GEAR_SHOP = {},
+    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino"},
+    BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
     KEEP_SEED = {},
-    KEEP_PLANT = {},
     KEEP_PLANT_RARITY = {"Secret", "Limited"},
-    KEEP_BRAINROT = {},
-    KEEP_BRAINROT_WEIGHT = math.huge,  -- Number
-    KEEP_BRAINROT_MONEY_PER_SECOND = math.huge,  -- Number
-    KEEP_BRAINROT_RARITY = {"Secret", "Limited"},
+    KEEP_BRAINROT_MONEY_PER_SECOND = 2000,  -- Number
+    KEEP_BRAINROT_RARITY = {},
 
-    SELL_BRAINROT_DELAY = 20,
-    SELL_PLANT_DELAY = 20,
-
-    -- Webhook
-    BRAINROT_WEBHOOK_URL = "https://discord.com/api/webhooks/1280850957747621918/kk5-Ncum_fSh2IiYC0j4rqmow2Qqre6gOwr4S949BO3MXcGtVEUWLtmX2BijwM44ALm-",
-    DISCORD_ID = "",
-    NOTIFY_RARITY = { "Secret", "Limited" },
-    NOTIFY_NAME = {},
-    NOTIFY_WEIGHT = math.huge,
-    NOTIFY_MONEY_PER_SECOND = math.huge,
-    WEBHOOK_NOTE = "",
-    SHOW_WEBHOOK_USERNAME = true,
-    SHOW_WEBHOOK_JOBID = true,
+    SELL_BRAINROT_DELAY = 30,
+    SELL_PLANT_DELAY = 30,
 }
+
+if getgenv().pvbConfig then
+    for key, value in pairs(publicConfig) do
+        getgenv().pvbConfig[key] = value
+    end
+end
