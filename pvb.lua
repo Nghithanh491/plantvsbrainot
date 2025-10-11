0191fefb34b320c85f71ccdde708ca37774c4e18
Nghithanh491 <nghithanhau@gmail.com>
2025-10-11 20:39:19 +0700

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -1,21 +1,18 @@
-local publicConfig = {  
-  AUTO_UPDATE_RESTART = true,
-    MAX_FPS = 3,  -- This will override setfpscap()
+local publicConfig = {
+    AUTO_UPDATE_RESTART = true,
+    MAX_FPS = 3, -- This will override setfpscap()
     LOW_CPU = true,
-    MAX_REBIRTH = 99,  -- Stop rebirth at set amount
-    FORCE_REBIRTH_IGNORE_KEEP_BRAINROT = true,  -- Ignore KEEP_BRAINROT related config until max rebirth
-    FROST_GRENADE_TARGET_MAX_HP = 100000,  -- Use frost grenade 100k+ hp brainrot
-    CONSUME_POTION_EVENT = {"Frozen", "Golden", "Rainbow", "Galactic", "Underworld", "UpsideDown", "Volcano"},
-    PLANT_SECRET_LIMITED_SEED_EVENT = {"Frozen", "Golden", "Rainbow", "Galactic", "Underworld", "UpsideDown", "Volcano"},
-    
-    OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
-    FUSE_PLANT = {"Mr Carrot", "Watermelon", "Eggplant", "Dragon Fruit", "Sunflower", "Pumpkin"},  -- Auto keep (favorite) & fuse required plant + brainrot
+    MAX_REBIRTH = 99, -- Stop rebirth at set amount
+    FORCE_REBIRTH_IGNORE_KEEP_BRAINROT = true, -- Ignore KEEP_BRAINROT related config until max rebirth
+    FROST_GRENADE_TARGET_MAX_HP = 100000, -- Use frost grenade 100k+ hp brainrot
 
-    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot","Tomade Torelli", "Tomatrio", "Shroombino", "Mango", "King Limone",},
+    OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
+    FUSE_PLANT = {"Mr Carrot", "Watermelon", "Eggplant", "Dragon Fruit", "Sunflower", "Pumpkin"}, -- Auto keep & fuse required plant + brainrot
+
+    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino", "Mango", "King Limone"},
     BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
 
-    -- Keep = Favorite (Prevent sell)
-     KEEP_SEED = {},
+    KEEP_SEED = {},
     KEEP_PLANT_RARITY = {"Secret", "Limited"},
     KEEP_BRAINROT = {"Bredda Ratto", "Squalo Cavallo ", "Dragon Cannelloni", "Gattolini Owlini", "Pot Hotspot", "Crazylone Pizaione", "La Tomatoro", "Los Tralaleritos", "Los Mr Carrotitos"},
     KEEP_BRAINROT_MONEY_PER_SECOND = 20000, -- Number
