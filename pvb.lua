e0315655d92791735d9cd292933c8e997b799035
Nghithanh491 <nghithanhau@gmail.com>
2025-11-11 21:28:04 +0700

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -2,24 +2,25 @@ local publicConfig = {
  AUTO_UPDATE_RESTART = true,
     MAX_FPS = 3,  -- This will override setfpscap()
     LOW_CPU = true,
-    MAX_REBIRTH = 6,  -- Stop rebirth at set amount
-    USE_XP_BOTTLE = true,  -- Use XP Bottle on highest damage plant
+    MAX_REBIRTH = 10,  -- Stop rebirth at set amount
+    USE_XP_BOTTLE = false,  -- Use XP Bottle on highest damage plant
     FROST_GRENADE_TARGET_MAX_HP = 100000,  -- Use frost grenade 100k+ hp brainrot
     CONSUME_POTION_EVENT = {"Frozen", "Golden", "Rainbow", "Galactic", "Underworld", "UpsideDown", "Volcano"},
     PLANT_SECRET_LIMITED_SEED_EVENT = {"Frozen", "Golden", "Rainbow", "Galactic", "Underworld", "UpsideDown", "Volcano"},
     
     OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
-    OPEN_CARD_PACK = {"Base", "Shiny Base"},
+    OPEN_CARD_PACK = {"Base", "Shiny Base", "Halloween"},
     MERGE_CARD_RARITY = {"Common", "Uncommon", "Rare", "Epic", "Legendary"},
     USE_PLANT_EXTRACTOR_RARITY = {"Common", "Uncommon", "Rare", "Epic", "Legendary"},
-    FUSE_PLANT = {"Watermelon", "Eggplant", "Dragon Fruit", "Sunflower", "Pumpkin"},  -- Auto keep (favorite) & fuse required plant + brainrot
+    FUSE_PLANT = {"Mr Carrot", "Watermelon", "Eggplant", "Dragon Fruit", "Sunflower", "Pumpkin"},  -- Auto keep (favorite) & fuse required plant + brainrot
+    MERGE_MUTATION_NAME = {},  -- Can Add ANY Plant/Brainrot Names
 
     BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino", "Mango", "King Limone", "Starfruit"},
     BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
 
     -- Keep = Favorite (Prevent sell)
     KEEP_SEED = {},  -- Plant Names
-    KEEP_PLANT_RARITY = {"Secret"}, --  "Limited"
+    KEEP_PLANT_RARITY = {"Secret",},
     KEEP_BRAINROT_MONEY_PER_SECOND = 40000,  -- 20000 means keep $20+k/s brainrot
     KEEP_BRAINROT_RARITY = {}, 
 
